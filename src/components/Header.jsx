0ea7ba0df95cb92cdba127f5ff52935bec054b87
Shorty <NoEmail@github.com>
2026-01-02 12:22:58 +0800

    Describe your change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,9 @@
-
 //  src/components/Header.jsx
 
 export default function Header() {
   return (
     <header>
-      <h1 style={{ color: "#B19CD9" }}>My Website Portfolio (IG)</h1>
+      <h1 style={{ color: "#B19CD9" }}>My Website Portfolio</h1>
     </header>
   );
 }
